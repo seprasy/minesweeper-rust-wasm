7c7bb78d824c6b0bc6e54e91824504940d02dc88
seprasy <sepraselva@gmail.com>
2022-07-13 02:19:42 +0800

    display number of neighbours with bombs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 extern "C" {
     pub fn fillRect(x: i32, y: i32, w: i32, h: i32, r: i32, g: i32, b: i32, a: i32);
     pub fn fillNum(x: i32, y: i32, num: i32);
@@ -35,14 +34,13 @@ pub struct Minesweeper {
     cols: usize,
     cells: Vec<Vec<Cell>>,
     cell_border: i32,
-    bomb_percent: usize
+    bomb_percent: usize,
 }
 
 impl Minesweeper {
-    fn get_empty_cell(&mut self) -> (usize,usize) {
-
+    fn get_empty_cell(&mut self) -> (usize, usize) {
         let total_cells = self.rows * self.cols;
-        let num = unsafe {rand(total_cells)};
+        let num = unsafe { rand(total_cells) };
         let row = num / self.cols;
         let col = num % self.cols;
 
@@ -58,11 +56,35 @@ impl Minesweeper {
         let mut no_of_bombs = total_cells * self.bomb_percent / 100;
 
         while no_of_bombs > 0 {
-            let (row,col) = self.get_empty_cell();
+            let (row, col) = self.get_empty_cell();
             self.cells[row][col].cell_type = CellType::Bomb;
             no_of_bombs -= 1;
         }
     }
+
+    fn neighbours_with_bombs(&self, row: usize, col: usize) -> i32 {
+        let mut bombs = 0;
+        for row_offset in -1..2 {
+            for col_offset in -1..2 {
+                if !(col_offset == 0 && row_offset == 0) {
+                    if !(row as i32 + row_offset >= self.rows as i32
+                        || col as i32 + col_offset >= self.cols as i32
+                        || row as i32 + row_offset < 0
+                        || col as i32 + col_offset < 0)
+                    {
+                        if self.cells[(row as i32+ row_offset) as usize][(col as i32 + col_offset) as usize]
+                            .cell_type
+                            == CellType::Bomb
+                        {
+                            bombs += 1;
+                        }
+                    }
+                }
+            }
+        }
+        return bombs;
+    }
+
     fn render(&self) {
         for row in 0..self.rows {
             for col in 0..self.cols {
@@ -80,7 +102,7 @@ impl Minesweeper {
                         );
                     },
                     CellType::None => unsafe {
-                        fillRect(
+                           fillRect(
                             col as i32 * self.cell_size + self.cell_border,
                             row as i32 * self.cell_size + self.cell_border,
                             self.cell_size - self.cell_border,
@@ -90,6 +112,14 @@ impl Minesweeper {
                             200,
                             255,
                         );
+                        let neighbouring_bombs = self.neighbours_with_bombs(row, col);
+                        if neighbouring_bombs > 0 {
+                            fillNum(
+                                (col as i32 * self.cell_size )+ ( self.cell_size / 4) ,
+                                (row as i32 * self.cell_size ) + (self.cell_size *3 / 4),
+                                neighbouring_bombs,
+                            );
+                        } 
                     },
                 }
             }
@@ -119,7 +149,7 @@ pub extern "C" fn init(screen_height: i32, screen_width: i32, cell_size: i32) ->
         cols: cols,
         cells: cells,
         cell_border: 2,
-        bomb_percent: 15
+        bomb_percent: 15,
     };
 
     game.generate_bombs();
@@ -128,7 +158,7 @@ pub extern "C" fn init(screen_height: i32, screen_width: i32, cell_size: i32) ->
 
 #[no_mangle]
 pub extern "C" fn render(game: *mut Minesweeper) {
-    let mut game = unsafe { &mut *game };
+    let game = unsafe { &mut *game };
 
     unsafe {
         fillRect(0, 0, game.screen_width, game.screen_height, 20, 20, 20, 255);
