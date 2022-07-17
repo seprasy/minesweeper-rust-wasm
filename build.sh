0e46e13266c286228976628fb81410df26de6c19
seprasy <sepraselva@gmail.com>
2022-07-17 22:40:31 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rustup target add wasm32-unknown-unknown
+cargo install wasm-gc
 cargo build --target wasm32-unknown-unknown --release
 wasm-gc target/wasm32-unknown-unknown/release/minesweeper_rust_wasm.wasm
 cp target/wasm32-unknown-unknown/release/minesweeper_rust_wasm.wasm .
